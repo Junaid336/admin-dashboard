b37473b8568a51f2323a9b3d848ed7df3cbbd3d3
Junaid336 <junaix003d@gmail.com>
2023-08-09 20:09:14 +0500

    calendar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
+import Calendar from "./scenes/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -24,6 +25,7 @@ function App() {
               <Route path="/team" element={<Team />}/>
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
+              <Route path="/calendar" element={<Calendar />} />
             </Routes>
           </main>
         </div>
